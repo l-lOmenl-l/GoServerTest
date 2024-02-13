bfd822cf6fc900da570f7814b90dcc1b7ca97145
Nikolai <pushkarev.n@e-1.ru>
2024-02-13 09:30:09 +0300

    update struct user allusers

--- internal/repository/users_postgres.go
+++ internal/repository/users_postgres.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"example/web-servise-gin/internal/domain"
 	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -16,14 +17,14 @@ func NewUsersPostgres(db *sqlx.DB) *UsersPostgres {
 
 func (r *UsersPostgres) GetDetail(user_id int) (domain.UserDetail, error) {
 	var userdetail domain.UserDetail
-	query := fmt.Sprintf("SELECT lastname, firstname, fathername, email, users.active, superuser, staff, registrationsdate, entrydate, salon.name as \"salon_name\", cities.name as \"cities_name\" " +
+
+	query := fmt.Sprintf("SELECT lastname, firstname, fathername, email, users.active, superuser, staff, registrationsdate, entrydate, salon.name as \"salon\", cities.name as \"cities\" " +
 		"FROM users " +
 		"inner join salon on users.salon_id=salon.id " +
 		"inner join cities on salon.city_id=cities.id " +
 		"where users.id=$1")
 
 	err := r.db.Get(&userdetail, query, user_id)
-
 	return userdetail, err
 }
 
@@ -31,13 +32,15 @@ func (r *UsersPostgres) GetAll() ([]domain.AllUsers, error) {
 
 	var allusers []domain.AllUsers
 
-	query := fmt.Sprintf("SELECT countries.name as \"country\", district.name as \"district\", regions.name as \"region\", cities.name as \"city\", salon.name as \"salon\", users.login as \"user\" " +
-		"FROM countries " +
-		"inner join district on district.country_id=countries.id " +
-		"inner join regions on regions.district_id=district.id " +
-		"inner join cities on cities.region_id=regions.id " +
-		"inner join salon on salon.city_id = cities.id " +
-		"inner join users on users.salon_id = salon.id ")
+	query := fmt.Sprintf("SELECT countries.id AS \"country_id\", countries.name AS \"country\", district.id AS \"district_id\"," +
+		"district.name AS \"district\", regions.id AS \"region_id\", regions.name AS \"region\"," +
+		"cities.id AS \"city_id\", cities.name AS \"city\", salon.id AS \"salon_id\", salon.name AS \"salon\", users.id AS \"user_id\", users.login AS \"user\" " +
+		"FROM users " +
+		"INNER JOIN salon ON users.salon_id=salon.id " +
+		"INNER JOIN cities ON salon.id=cities.id " +
+		"INNER JOIN regions ON regions.id=cities.id " +
+		"INNER JOIN district ON district.id=regions.id " +
+		"INNER JOIN countries ON countries.id=district.id")
 
 	err := r.db.Select(&allusers, query)
 
